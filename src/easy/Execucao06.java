5b6df4f61b80a6f25d3e3d1bb33809b78926f4b3
Ricardo <ricardonere@gmail.com>
2022-03-23 17:26:31 -0300

    Transformacao Exercicio06 Easy - metodo com retorno e com passagem de parametro

---
+++ src/easy/Execucao06.java
@@ -0,0 +1,8 @@
+package easy;
+
+public class Execucao06 {
+    public static void main(String[] args) {
+        Exercicio6 exercicio6 = new Exercicio6();
+        System.out.println( exercicio6.verificarAprovacao(5, 5));
+    }
+}
--- src/easy/Exercicio6.java
+++ src/easy/Exercicio6.java
@@ -1,36 +1,28 @@
 package easy;
 
-
-
 import javax.swing.JOptionPane;
 
 public class Exercicio6 {
-    public static void main(String[] args) {
-        int nota1, nota2;
+    public String verificarAprovacao(int nota1, int nota2) {
+        
         int media;
-       
-        nota1 = Integer.parseInt(JOptionPane.showInputDialog("Digite a primeira nota"));
-        nota2 = Integer.parseInt(JOptionPane.showInputDialog("Digite a segunda nota"));
 
-media = (nota1+nota2) /2;
-
-
-if (media > 5) {
-     System.out.println( "Aprovado" +media); 
-}
-
-if (media < 5){
-System.out.println( "Reprovado" +media); 
-}
-
-    if (media == 5){
-
-    System.out.println( "Exame" +media); 
-}
     
+        media = (nota1 + nota2) / 2;
 
+        if (media > 5) {
+            return("Aprovado" + media);
+        }
 
+        if (media < 5) {
+            return("Reprovado" + media);
+        }
 
+        if (media == 5) {
+
+            return("Exame" + media);
+        }
+        return null;
 
     }
 }
