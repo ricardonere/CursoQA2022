4311ab301a389df99eda3ce6aa95c8a36143d283
Ricardo <ricardonere@gmail.com>
2022-03-23 15:42:33 -0300

    Transformacao Exercicio05 Easy - metodo com retorno e com passagem de parametros

---
+++ src/easy/Execucao04.java
@@ -0,0 +1,11 @@
+package easy;
+
+public class Execucao04 {
+    public static void main(String[] args) {
+        Exercicio4 exercicio4 = new Exercicio4();
+        String numero = exercicio4.numeroDigitado(500);
+        System.out.println(numero);
+
+
+    }
+}
---
+++ src/easy/Execucao05.java
@@ -0,0 +1,13 @@
+package easy;
+
+import exemploMetodos.sysout;
+
+public class Execucao05 {
+    public static void main(String[] args) {
+        Exercicio5 exercicio5 = new Exercicio5();
+        System.out.println(exercicio5.calcularSoma(5, 10, 15));
+        System.out.println(exercicio5.calcularSubtracao(5, 10, 15));
+        System.out.println(exercicio5.calcularMultiplicacao(5, 10, 15));
+        System.out.println(exercicio5.calcularMedia(5, 10, 15));
+    }
+}
