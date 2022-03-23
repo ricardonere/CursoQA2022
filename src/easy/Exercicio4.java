5031e1638ec2fee06db9ba373903493b25fd3ce2
Ricardo <ricardonere@gmail.com>
2022-03-23 14:26:28 -0300

    Transformação Exercicio 04 Easy - metodo com retorno e com passagem de parametros

--- src/easy/Exercicio4.java
+++ src/easy/Exercicio4.java
@@ -4,10 +4,11 @@ package easy;
 import javax.swing.JOptionPane;
 
 public class Exercicio4 {
-    public static void main(String[] args) {
-        int numero = Integer.parseInt(JOptionPane.showInputDialog("Digite um número"));
-
-        System.out.println(numero * 2);
-
+    public String numeroDigitado (int numero) {
+        
+        return ("O usuário digitou: "+numero*2); 
+           
     }
+
+    
 }
