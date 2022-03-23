8778106209181e9bac3ff1ba12c24f15e9e1a6ca
Ricardo <ricardonere@gmail.com>
2022-03-23 18:07:48 -0300

    Transformacao Exercico07 Easy - metodo com retorno e com passagem de parametros

--- src/easy/Exercicio7.java
+++ src/easy/Exercicio7.java
@@ -3,35 +3,35 @@ package easy;
 import javax.swing.JOptionPane;
 
 public class Exercicio7 {
-    public static void main(String[] args) {
-        int salario, inss;
+    public String calculoSalario(double salario,double inss) {
+
         salario = Integer.parseInt(JOptionPane.showInputDialog("Digite o valor do seu salario"));
 
-        if (salario <= 1045.00){
-            
-        inss = (int) (salario * 0.075);
-        System.out.println("O Valor que você de inss será: " + inss);
+        if (salario <= 1045.00) {
+
+            inss = (int) (salario * 0.075);
+            return ("O Valor que você de inss será: " + inss);
         }
         if (salario >= 1045.01 && salario <= 2089.60) {
             inss = (int) (salario * 0.09);
-            System.out.println(" O valor de inss será: " + inss);
+            return (" O valor de inss será: " + inss);
         }
-            if (salario >= 2089.61 && salario <= 3134.40){
-                
-            inss = (int) (salario * 0.12);
-            System.out.println("O valor de Inss será: " + inss);
-            }
-            if (salario >= 3134.41 && salario <= 6101.06){
-                
-            inss = (int) (salario * 0.14);
-            System.out.println("O Valor que você de inss será: " + inss);
-            }
-            if (salario >= 6101.07){
-                
-            inss = (int) 854.15;
-            System.out.println("O Valor que você de inss será: " + inss);
-            }
-        }
+        if (salario >= 2089.61 && salario <= 3134.40) {
 
+            inss = (int) (salario * 0.12);
+            return ("O valor de Inss será: " + inss);
+        }
+        if (salario >= 3134.41 && salario <= 6101.06) {
+
+            inss = (int) (salario * 0.14);
+            return ("O Valor que você de inss será: " + inss);
+        }
+        if (salario >= 6101.07) {
+
+            inss = (int) 854.15;
+            return ("O Valor que você de inss será: " + inss);
+        }
+        return null;
     }
 
+}
