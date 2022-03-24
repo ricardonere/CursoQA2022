d4270c95c9905674741ea7341a399c2bc160b29f
Ricardo <ricardonere@gmail.com>
2022-03-24 17:05:34 -0300

    Transformação Exercicio10 Easy - metodo com retorn o e com passagem de parametros

--- src/easy/Exercicio10.java
+++ src/easy/Exercicio10.java
@@ -3,16 +3,15 @@ package easy;
 import javax.swing.JOptionPane;
 
 public class Exercicio10 {
-    public static void main(String[] args) {
-        double valorInvestimento, taxaJuros = 0.05, valorJuros, valorTotal= 0.0;
+    public String valorInvestido(double valorInvestimento, double taxaJuros , double valorJuros, double valorTotal) {
+       
         valorInvestimento = Integer.parseInt(JOptionPane.showInputDialog("Digite o valor investido"));
         taxaJuros = 0.05 * 10; //0.50 ou 50%
         valorJuros = valorInvestimento * taxaJuros; // 500,00. valorTotal = valorTotal = valorJuros+valorInvestimento
         valorTotal = valorJuros+valorInvestimento;
 
-        System.out.println("Você investiu: "+valorInvestimento);
-        System.out.println("Rendeu : "+valorJuros + "de Juros");
-        System.out.println("O Valor total são : "+ valorTotal);
+        return("Você investiu: "+valorInvestimento+"Rendeu : "+valorJuros + "de Juros"+"O Valor total são : "+ valorTotal);
+        
         
 
 
