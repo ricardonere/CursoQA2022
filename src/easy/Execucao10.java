7b87b6fe06d01e15e201e4ee531a340e46620147
Ricardo <ricardonere@gmail.com>
2022-03-28 14:54:36 -0300

    Refatoração Exercicio02 Medium

---
+++ src/easy/Execucao10.java
@@ -0,0 +1,8 @@
+package easy;
+
+public class Execucao10 {
+    public static void main(String[] args) {
+        Exercicio10 exercicio10 = new Exercicio10();
+        System.out.println(exercicio10.valorInvestido(1000, 0.05, 500, 1500));
+    }
+}
--- src/medium/Exercicio02_medium.java
+++ src/medium/Exercicio02_medium.java
@@ -4,24 +4,25 @@ import javax.swing.JOptionPane;
 
 public class Exercicio02_medium {
 
-    public static void main(String[] args) {
+    public double valorInvestidoJurosCompostos (double valorInvestimento) {
 
-        double valorInvestimento, taxaJuros = 0.05, totalAtualizadoComJuros;
+        double taxaJuros = 0.05; double totalAtualizadoComJuros;
         int ano = 1, tempoInvestido = 10;
-        String valorInvestimentoStg =  JOptionPane.showInputDialog("Digite um valor de investimento: ");
-        valorInvestimento = Integer.parseInt(valorInvestimentoStg);
+        // String valorInvestimentoStg =  JOptionPane.showInputDialog("Digite um valor de investimento: ");
+        // valorInvestimento = Integer.parseInt(valorInvestimentoStg);
         totalAtualizadoComJuros = valorInvestimento;
 
         while (ano <= tempoInvestido) {
             totalAtualizadoComJuros = (totalAtualizadoComJuros * taxaJuros) + totalAtualizadoComJuros;
             ano++;
         } // fim while
-        System.out.println("O valor do investimento são: " 
-        + valorInvestimento);
-        System.out.println("O valor do Juros são: " + (totalAtualizadoComJuros - valorInvestimento));
+    //    return("O valor do investimento são: " + valorInvestimento);
+    //     System.out.println("O valor do Juros são: " + (totalAtualizadoComJuros - valorInvestimento));
 
-        System.out.println("O valor total do investimento: "+totalAtualizadoComJuros);
-
-
+        return totalAtualizadoComJuros-valorInvestimento;
+        // return tempoInvestido;
     }// fim main
+    public double calcularValorInvestido(double valorInvestimento, double valorJuros){
+        return valorInvestimento + valorJuros;
+    }
 }// fim da classe
