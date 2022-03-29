fea1c166237e1627909eaa100c95017297c4a4ba
Ricardo <ricardonere@gmail.com>
2022-03-29 17:24:17 -0300

    Refatoração Exercicio09 Medium

--- src/medium/Exercicio09_medium.java
+++ src/medium/Exercicio09_medium.java
@@ -2,42 +2,56 @@ package medium;
 
 import javax.swing.JOptionPane;
 
+import exemploMetodos.sysout;
+
 public class Exercicio09_medium {
-    public static void main(String[] args) {
-        
+    public void exibeNomeNumeros()  {
+       
         String vetorAlunos[] = new String[5];
-        String vetorNumeroAlunos[] = new String[5];
-        int i=0;
-        while (i<=4){
-            vetorAlunos[i] = JOptionPane.showInputDialog("Digite o nome");
-            vetorNumeroAlunos[i] = JOptionPane.showInputDialog("Digite o numero");
-            i++; 
-        }
-        i=0;
-        while (i<=4){
-            System.out.println(vetorAlunos[i]);
-            System.out.println(vetorNumeroAlunos[i]);
+        int vetorNumeroAlunos[] = new int[5];
+        int i =0;
+
+        while (i<5){
+        vetorAlunos[i] = recebeNomeAluno();
+        vetorNumeroAlunos[i] = recebeNumeroAluno();
+        i++;
+    }
+    
+imprimeNomeNumero(vetorAlunos,vetorNumeroAlunos);
+    
+    }
+
+    private void imprimeNomeNumero(String[] vetorAlunos, int[] vetorNumeroAlunos) {
+        int i=0;  
+
+        while (i < 5){
+            System.out.println("O nome do aluno é"  +  vetorAlunos[i] +"-"+ "O numero do aluno é"  +  vetorNumeroAlunos[i]);
             i++;
         }
-        // Declarar vetor do tipo texto: vetorAlunos[5];
-        // Declarar vetor do tipo inteiro: vetorNumeroAlunos[5];
-        // Declarar variável do tipo inteira: i=0;
-         
-        // Enquanto(i<=4)
-        //             vetorAlunos[i] = Ler();
-        // vetorNumeroAlunos[i] = Ler();
-        // i++;
-        // Fim Enquanto
-         
-        // i=0; //Reset do valor da variável
-         
-        // Enquanto(i<=4)
-        //             Exibir: vetorAlunos[i];
-        //             Exibir: vetorNumeroAlunos[i];
-        //             i++;
-        
-
-
 
     }
+    private int recebeNumeroAluno() {
+        return Integer.parseInt(JOptionPane.showInputDialog("Digite o numero do aluno"));
+    }
+
+    private String recebeNomeAluno() {
+        return JOptionPane.showInputDialog("Digite o nome do aluno");
+    }
+
+    
 }
+
+// Enquanto(i<=4)
+// vetorAlunos[i] = Ler();
+// vetorNumeroAlunos[i] = Ler();
+// i++;
+// Fim Enquanto
+
+// i=0; //Reset do valor da variável
+
+// Enquanto(i<=4)
+// Exibir: vetorAlunos[i];
+// Exibir: vetorNumeroAlunos[i];
+// i++;
+
+
